f239554e7a4f85f5e450b09554d124e0efc0fe29
ca-trindade <piratum.core@gmail.com>
2023-10-18 12:06:45 +0100

    added useRef

--- src/App.jsx
+++ src/App.jsx
@@ -32,13 +32,13 @@ function App() {
 
 //=== useRef ============================================
 
-//exemple 1
+//example 1
 
     const numberOfRenders = useRef(0);
     useEffect(() => {
         numberOfRenders.current = numberOfRenders.current + 1;
     });
-//exemple 2
+//example 2
     const inputRef = useRef();
 const focusInput = () => {
     inputRef.current.focus()
@@ -75,10 +75,10 @@ const focusInput = () => {
 
             <div>
                 <h3>-useRef-</h3>
-                <p>Exemple: 1</p>
+                <p>Example: 1</p>
                 <input ref={inputRef} value={numberOfReloads} onChange={(e) => setnumberOfReloads(e.target.value)}/>
                 <h2>This input with "{numberOfReloads}" have {numberOfRenders.current} reloads</h2>
-                <p>Exemple: 2</p>
+                <p>Example: 2</p>
                 <button onClick={focusInput}>useRef also used to focus</button>
             </div>
 
